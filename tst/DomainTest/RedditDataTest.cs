174722bcc9c22edbef8c28258b6a499f14f2022c
Stan Bennett <stanley.bennett82@gmail.com>
2020-10-23 16:56:17 -0500

    Add coverage. Refactor nant coverage tasks.

---
+++ tst/DomainTest/RedditDataTest.cs
@@ -0,0 +1,42 @@
+﻿using Domain;
+using NUnit.Framework;
+
+namespace DomainTest
+{
+    [TestFixture]
+    public class RedditDataTest
+    {
+
+        private RedditData Data;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Data = new RedditData();
+        }
+
+        #region Blue Sky Tests
+
+        [Test]
+        public void TestBlueSky_Coverage()
+        {
+            // pre-conditions
+            Assert.IsNull(Data.Contents);
+
+            // exercise
+            Data.Contents = new RedditInfo[] { new RedditInfo() };
+
+            // post-conditions
+            Assert.AreEqual(1, Data.Contents.Length);
+        }
+
+        #endregion
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+    }
+}
