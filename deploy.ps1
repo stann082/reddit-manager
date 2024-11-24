812d4a03f4de7d093069f5884137d3b917ff83dc
Stan Bennett <stanley.bennett82@gmail.com>
2024-11-24 15:11:53 -0600

    Remove old deploy script

--- deploy.ps1
+++
@@ -1,3 +0,0 @@
-remove-item .\pub\ -recurse -erroraction silentlycontinue
-dotnet publish .\src\app\app.csproj -c Release -o pub
-copy-item -path .\pub\app.exe -destination $env:APPDATA\utils\reddit.exe -verbose
